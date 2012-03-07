6e4092a66f061ae0c196d4bbdad72f7bfca1c6c9
dribnet <tom@sixdozen.com>
2012-03-07 04:50:25 -0800

    Updated project.clj to cascalog 1.8.6 + clojure 1.3.0
    Also updated :namespaces to :aot for more recent lein syntax.
    Removed now unnecessary clojure-contrib dependency.
    
    Tested with hadoop 1.0.1 - provides identical output
    as previous version.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject cascalog-demo "1.0.0-SNAPSHOT"
   :source-path "src/clj"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [cascalog "1.3.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [cascalog "1.8.6"]
                  ]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]
-  :namespaces [cascalog-demo.demo])
+  :aot [cascalog-demo.demo])
